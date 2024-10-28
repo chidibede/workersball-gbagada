db7c8a681d9727336213f86096041508247a2fee
Chidi Enwereji <chidibede@gmail.com>
2024-10-28 13:33:30 +0100

    add way to send the mail

--- src/components/ManuallySendEmail.jsx
+++ src/components/ManuallySendEmail.jsx
@@ -12,31 +12,39 @@ function ManuallySendEmail() {
   };
 
   return (
-    <div className="w-[50%] justify-center items-center p-24">
-      <input
-        type="text"
-        placeholder="First Name"
-        className="border p-3 w-full rounded-md"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Email"
-        className="border p-3 w-full rounded-md"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="active or inactive"
-        className="border p-3 w-full rounded-md"
-        onChange={(e) => setIsActive(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="code"
-        className="border p-3 w-full rounded-md"
-        onChange={(e) => setIsActive(e.target.value)}
-      />
+    <div className="space-y-4">
+      <div className="w-[50%] justify-center items-center px-24 py-12">
+        <input
+          type="text"
+          placeholder="First Name"
+          className="border p-3 w-full rounded-md mb-4"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          className="border p-3 w-full rounded-md mb-4"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="active or inactive"
+          className="border p-3 w-full rounded-md mb-4"
+          onChange={(e) => setIsActive(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="code"
+          className="border p-3 w-full rounded-md"
+          onChange={(e) => setCode(e.target.value)}
+        />
+      </div>
+      <button
+        className="bg-blue-500 text-white p-4 w-30 rounded-md hover:bg-blue-400 ml-24"
+        onClick={handleSendEmail}
+      >
+        Send Email
+      </button>
     </div>
   );
 }
